96cd680521f3e21a7bcbe3b3361e7917eb2a818b
magnodf7 <magno.brunodf7@gmail.com>
2024-05-10 23:40:35 -0300

    iniciando conta banco

---
+++ contaBanco/src/Banco.java
@@ -0,0 +1,17 @@
+public class Banco {
+    public static void main(String[] args) throws Exception {
+        
+      Cliente cliente = new Cliente();
+      
+      System.err.println("nome do cliente " + cliente.nome);
+
+      System.err.println("idade do cliente " + cliente.agencia);
+
+      System.err.println("salario " + cliente.conta);
+
+      System.err.println("salario " + cliente.saldo);
+
+      
+
+    }
+}
---
+++ contaBanco/src/Cliente.java
@@ -0,0 +1,25 @@
+public class Cliente {
+    String nome= "PedroBarros";
+    String agencia= "caixa casa amarela";
+    int conta = 1234;
+    double saldo =1.458
+
+   /*  public void nome(){
+        System.out.println("O cliente");
+    }
+
+    
+    public void outro(){
+        System.out.println("O cliente");
+    }
+    //continuar com os metodos
+
+    public void salarioAumentou(){
+        System.out.println("salario ");
+    }
+    
+    public void salarioAbaixou(){
+        System.out.println("salario ");
+    }*/
+}
+
